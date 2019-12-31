677dc47613796417d1f409132e51490e4b99ae87
traduong1998 <quangtra72@gmail.com>
2019-12-31 21:31:13 +0700

    Chức năng đăng ký

--- src/main/java/com/qnu/service/IFilmService.java
+++ src/main/java/com/qnu/service/IFilmService.java
@@ -1,8 +1,13 @@
 package com.qnu.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
 import com.qnu.dto.FilmDTO;
 
 public interface IFilmService {
 	FilmDTO findById(long id);
 	FilmDTO save(FilmDTO dto);
+	List<FilmDTO> findAll(Pageable pageable);
 }
